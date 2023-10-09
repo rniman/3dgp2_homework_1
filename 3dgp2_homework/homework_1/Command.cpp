b82cac7e04a74387e0861e3a972c6c9379126961
Shin Min Geun <shinmg00@gmail.com>
2023-10-09 18:27:43 +0900

    명령 패턴으로 변경 시작

---
+++ 3dgp2_homework/homework_1/Command.cpp
@@ -0,0 +1,75 @@
+#include "stdafx.h"
+#include "Command.h"
+#include "Object.h"
+#include "Player.h"
+
+void CForwardCommand::execute(CGameObject& gameObject)
+{
+	CPlayer* player = dynamic_cast<CPlayer*>(&gameObject);
+	XMFLOAT3 shift = Vector3::Add(XMFLOAT3(0.0f, 0.0f, 0.0f), player->GetLook(), 0.25f);
+	player->Move(shift, true);
+}
+
+void CBackwardCommand::execute(CGameObject& gameObject)
+{
+	CPlayer* player = dynamic_cast<CPlayer*>(&gameObject);
+	XMFLOAT3 shift = Vector3::Add(XMFLOAT3(0.0f, 0.0f, 0.0f), player->GetLook(), -0.25f);
+	player->Move(shift, true);
+}
+
+void CRightCommand::execute(CGameObject& gameObject)
+{
+	CPlayer* player = dynamic_cast<CPlayer*>(&gameObject);
+	XMFLOAT3 shift = Vector3::Add(XMFLOAT3(0.0f, 0.0f, 0.0f), player->GetRight(), 0.25f);
+	player->Move(shift, true);
+}
+
+void CLeftCommand::execute(CGameObject& gameObject)
+{
+	CPlayer* player = dynamic_cast<CPlayer*>(&gameObject);
+	XMFLOAT3 shift = Vector3::Add(XMFLOAT3(0.0f, 0.0f, 0.0f), player->GetRight(), -0.25f);
+	player->Move(shift, true);
+}
+
+void CCameraRotateYCommand::execute(CGameObject& gameObject)
+{
+	// 윈도우 창이 하나라면 필요없다.
+	//if (GetCapture() != CBaseInputHandler::GetHWND())
+	//	return;
+
+	float deltaX, deltaY;
+
+	POINT ptCursorPos;
+	SetCursor(nullptr);
+	GetCursorPos(&ptCursorPos);
+	deltaX = (float)(ptCursorPos.x - CBaseInputHandler::m_sptOldCursorPos.x) / 3.0f;
+	deltaY = (float)(ptCursorPos.y - CBaseInputHandler::m_sptOldCursorPos.y) / 3.0f;
+	SetCursorPos(CBaseInputHandler::m_sptOldCursorPos.x, CBaseInputHandler::m_sptOldCursorPos.y);
+
+	CPlayer* player = dynamic_cast<CPlayer*>(&gameObject);
+	player->Rotate(deltaY, deltaX, 0.0f);
+}
+
+UCHAR CBaseInputHandler::m_spKeysBuffer[] = {};
+POINT CBaseInputHandler::m_sptOldCursorPos = { 0, 0 };
+HWND CBaseInputHandler::m_shWnd;
+
+bool CBaseInputHandler::IsPressed(ButtonType buttonType)
+{
+	if (CBaseInputHandler::m_spKeysBuffer[m_mapButtonToKey[buttonType]] & 0xF0)
+	{
+		return true;
+	}
+	return false;
+}
+
+void CPlayerInputHandler::HandleInput(CGameObject& gameObject)
+{
+	if (IsPressed(ButtonType::BUTTON_MOVE_FORWARD)) button_forward->execute(gameObject);
+	if (IsPressed(ButtonType::BUTTON_MOVE_BACKWARD)) button_backward->execute(gameObject);
+	if (IsPressed(ButtonType::BUTTON_MOVE_RIGHT)) button_right->execute(gameObject);
+	if (IsPressed(ButtonType::BUTTON_MOVE_LEFT)) button_left->execute(gameObject);
+	if (IsPressed(ButtonType::BUTTON_CAMERA_ROTATE_Y)) button_camera_rotate_y->execute(gameObject);
+}
+
+
---
+++ 3dgp2_homework/homework_1/Command.h
@@ -0,0 +1,115 @@
+#pragma once
+
+class CGameObject;
+
+enum class ButtonType
+{
+	BUTTON_MOVE_FORWARD,
+	BUTTON_MOVE_BACKWARD,
+	BUTTON_MOVE_RIGHT,
+	BUTTON_MOVE_LEFT,
+	BUTTON_CAMERA_ROTATE_Y
+};
+
+class CBaseCommand
+{
+public:
+	virtual ~CBaseCommand() {}
+	virtual void execute(CGameObject& gameObject) = 0;
+};
+
+class CForwardCommand : public CBaseCommand
+{
+public:
+	virtual ~CForwardCommand() {}
+	virtual void execute(CGameObject& gameObject) override;
+};
+
+class CBackwardCommand : public CBaseCommand
+{
+public:
+	virtual ~CBackwardCommand() {}
+	virtual void execute(CGameObject& gameObject) override;
+};
+
+class CRightCommand : public CBaseCommand
+{
+public:
+	virtual ~CRightCommand() {}
+	virtual void execute(CGameObject& gameObject) override;
+};
+
+class CLeftCommand : public CBaseCommand
+{
+public:
+	virtual ~CLeftCommand() {}
+	virtual void execute(CGameObject& gameObject) override;
+};
+
+class CCameraRotateYCommand : public CBaseCommand
+{
+public:
+	virtual ~CCameraRotateYCommand() {}
+	virtual void execute(CGameObject& gameObject) override;
+};
+
+
+class CBaseInputHandler
+{
+public:
+	CBaseInputHandler()
+	{
+	}
+
+	virtual ~CBaseInputHandler()
+	{
+	}
+
+	bool IsPressed(ButtonType buttonType);
+	virtual void HandleInput(CGameObject& gameObject) = 0;
+
+	void ChangeKey() {};
+
+	// interface
+	void SetHWND(HWND hWnd) { m_shWnd = hWnd; };
+	static HWND GetHWND() { return m_shWnd; };
+
+	// static member variable
+	static UCHAR m_spKeysBuffer[256];
+	static POINT m_sptOldCursorPos;
+private:
+	static HWND	 m_shWnd;
+
+protected:
+	std::unordered_map<ButtonType, WORD> m_mapButtonToKey;
+};
+
+class CPlayerInputHandler : public CBaseInputHandler
+{
+public:
+	CPlayerInputHandler()
+		: CBaseInputHandler()
+	{
+		m_mapButtonToKey[ButtonType::BUTTON_MOVE_FORWARD] = VK_UP;
+		m_mapButtonToKey[ButtonType::BUTTON_MOVE_BACKWARD] = VK_DOWN;
+		m_mapButtonToKey[ButtonType::BUTTON_MOVE_RIGHT] = VK_RIGHT;
+		m_mapButtonToKey[ButtonType::BUTTON_MOVE_LEFT] = VK_LEFT;
+		m_mapButtonToKey[ButtonType::BUTTON_CAMERA_ROTATE_Y] = VK_LBUTTON;
+
+		button_forward = make_unique<CForwardCommand>();
+		button_backward = make_unique<CBackwardCommand>();
+		button_right = make_unique<CRightCommand>();
+		button_left = make_unique<CLeftCommand>();
+		button_camera_rotate_y = make_unique<CCameraRotateYCommand>();
+	}
+
+	virtual ~CPlayerInputHandler() {};
+	virtual void HandleInput(CGameObject& gameObject) override;
+
+private:
+	std::unique_ptr<CBaseCommand> button_forward;
+	std::unique_ptr<CBaseCommand> button_backward;
+	std::unique_ptr<CBaseCommand> button_right;
+	std::unique_ptr<CBaseCommand> button_left;
+	std::unique_ptr<CBaseCommand> button_camera_rotate_y;
+};
